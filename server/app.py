82e4e67cb9fa97e9a779ddaa2029ef95788d6860
Yueying Liu <648339108@qq.com>
2019-04-12 00:02:47 -0400

    some basic logic to annotations, but bad request

--- server/app.py
+++ server/app.py
@@ -2,17 +2,29 @@ import flask
 from flask import Flask
 from flask import request
 import json
+import datetime
 
 app = Flask(__name__)
 
 with open('paper_list.json', 'r') as f:
     paper_list = json.loads(f.read())['papers']
 
+with open('annotations.json', 'r') as a:
+    ann_list = json.loads(a.read())['annotations']
+
 
 def simplify(str1):
     str1 = "".join([x if x.isalnum() else ' ' for x in str1])
     return str1.lower()
 
+def refresh_ann(paper_id, ann_type):
+    response_list = []
+    for annotations in ann_list:
+        if annotations['id'] == paper_id:
+            for ann in annotations[ann_type]:
+                response_list.append(ann)
+    response = flask.jsonify({"annotations": response_list})
+    return response
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -55,5 +67,46 @@ def get():
     response = flask.jsonify({
         "paper": None
     })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/doc-ann', methods=['GET', 'POST'])
+def process_doc_annotations():
+    paper_id = int(request.args['id'])
+    for annotations in ann_list:
+        if annotations['id'] == paper_id:
+            if request.method == 'GET':
+                response = refresh_ann(paper_id, "document")
+                response.headers.add('Access-Control-Allow-Origin', '*')
+                return response
+            if request.method == 'POST':
+                id = paper["document"].length
+                author = request.args['author']
+                type = request.args['type']
+                content = request.args['content']
+                upvote = 0
+                downvote = 0
+                timestamp = datetime.datetime
+
+                paper["document"].append({"id": id, "author": author, "type": type, "content": content,
+                                          "upvote": upvote, "downvote": downvote, "timestamp": timestamp, "answer": []})
+    response = refresh_ann(paper_id, "document")
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/doc-ann-vote', methods=['POST'])
+def vote():
+    paper_id = int(request.args['id'])
+    for annotations in ann_list:
+        if annotations['id'] == paper_id:
+            ann_type = request.args['ann_type']
+            ann_id = int(request.args['ann_id'])
+            vote_type = request.args['vote_type']
+            for doc_ann in annotations[ann_type]:
+                if doc_ann['id'] == ann_id:
+                    doc_ann[vote_type] += 1
+    response = refresh_ann(paper_id, ann_type)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
